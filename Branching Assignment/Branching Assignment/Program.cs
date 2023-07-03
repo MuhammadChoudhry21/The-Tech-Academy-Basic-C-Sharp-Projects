6add0bef67bb1245b116606c9cef5de264c349f7
MuhammadChoudhry21 <fazal.zoom@gmail.com>
2023-07-03 14:42:28 -0400

    Fixed

--- Branching Assignment/Branching Assignment/Program.cs
+++ Branching Assignment/Branching Assignment/Program.cs
@@ -25,32 +25,34 @@ namespace Branching_Assignment
             else
             {
                 Console.WriteLine("What's the width of your package ?"); // Prints out this string
-            }
-            int packageWidth = Convert.ToInt32(Console.ReadLine()); // Stores user input into a variable
 
-            Console.WriteLine("What's the height of your package ?"); // Prints out this string
-            int packageHeight = Convert.ToInt32(Console.ReadLine()); // Stores user input into a variable
+                int packageWidth = Convert.ToInt32(Console.ReadLine()); // Stores user input into a variable
 
-            Console.WriteLine("What's the length of your package ?"); // Prints out this string
-            int packageLength = Convert.ToInt32(Console.ReadLine()); // Stores user input into a variable
+                Console.WriteLine("What's the height of your package ?"); // Prints out this string
+                int packageHeight = Convert.ToInt32(Console.ReadLine()); // Stores user input into a variable
 
-            // This if statement will check if the total dimensions together is less than 50 or not
-            // If it's greater, the program ends & we print the string
-            if (packageHeight + packageLength + packageWidth > 50)
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express."); // Prints out this string
-                Console.ReadLine();
-            }
+                Console.WriteLine("What's the length of your package ?"); // Prints out this string
+                int packageLength = Convert.ToInt32(Console.ReadLine()); // Stores user input into a variable
 
-            // If it's less than 50, then the program continues with this else if statement
-            else if (packageHeight + packageLength + packageWidth < 50)
-            
-            {
-                int finalResults = (packageWidth * packageHeight * packageLength) * packageWeight / 100;
+                // This if statement will check if the total dimensions together is less than 50 or not
+                // If it's greater, the program ends & we print the string
+                if (packageHeight + packageLength + packageWidth > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express."); // Prints out this string
+                    Console.ReadLine();
+                }
 
-                Console.WriteLine("Your estimated total for shipping this package is: " + "$" + finalResults + ".00"); // Prints out this string & variable
-                Console.WriteLine("Thank you!");
-                Console.ReadLine();
+                // If it's less than 50, then the program continues with this else if statement
+                else if (packageHeight + packageLength + packageWidth < 50)
+
+                {
+                    int finalResults = (packageWidth * packageHeight * packageLength) * packageWeight / 100;
+
+                    Console.WriteLine("Your estimated total for shipping this package is: " + "$" + finalResults + ".00"); // Prints out this string & variable
+                    Console.WriteLine("Thank you!");
+                    Console.ReadLine();
+                }
+
             }
         }
     }
