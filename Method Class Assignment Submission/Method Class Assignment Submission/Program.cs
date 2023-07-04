2d8158b745ed473c86337064fb642335b6eaaca0
MuhammadChoudhry21 <fazal.zoom@gmail.com>
2023-07-04 16:08:22 -0400

    uploading method class assignment

---
+++ Method Class Assignment Submission/Method Class Assignment Submission/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myMethods mathOp = new myMethods(); // Instantiating the class
+
+            mathOp.math(50, 100); // Calling the method & passing 2 numbers
+                                  // Also specifies by parameters by name
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method Class Assignment Submission/Method Class Assignment Submission/myMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment_Submission
+{
+    public class myMethods
+    {
+        // Creating a void method with 2 integer parameters
+        public void math(int num1, int num2)
+        {
+            int num1Result = num1 + 20; // Performing a math operation on the first parameter
+            Console.WriteLine(num1Result); // Printing out the result
+            Console.WriteLine(num2); // Printing out the second parameter
+            Console.WriteLine("The parameters are num1 & num2"); // Specifying the parameters by name
+        }
+    }
+}
