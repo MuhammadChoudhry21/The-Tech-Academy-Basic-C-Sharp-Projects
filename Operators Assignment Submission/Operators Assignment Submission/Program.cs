a95731a752d5dd9f1746851135bf260f3944781f
MuhammadChoudhry21 <fazal.zoom@gmail.com>
2023-07-05 15:07:51 -0400

    Update Program.cs

--- Operators Assignment Submission/Operators Assignment Submission/Program.cs
+++ Operators Assignment Submission/Operators Assignment Submission/Program.cs
@@ -14,7 +14,7 @@ namespace Operators_Assignment_Submission
             Employee employee1 = new Employee() { ID = 1, firstName = "Roger", lastName = "Lum" };
             Employee employee2 = new Employee() { ID = 2, firstName = "Marie", lastName = "Hill" };
 
-            // Trying to compare ID rpoperty of each object
+            // Comparing ID rpoperty of each object
             Console.WriteLine(employee1.ID == employee2.ID);
             Console.ReadLine();
         }
