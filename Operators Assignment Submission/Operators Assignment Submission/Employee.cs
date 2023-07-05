fb5096a980e6adbfe630d819442bbd46b2fb8857
MuhammadChoudhry21 <fazal.zoom@gmail.com>
2023-07-05 14:45:59 -0400

    submission

---
+++ Operators Assignment Submission/Operators Assignment Submission/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment_Submission
+{
+    public class Employee //Creating an Employee class
+    {
+        // Giving it properties
+        public string ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        // Creating == and != overload operator
+        public static Employee operator== (Employee employeeOne, Employee employeeTwo)
+        {
+            Console.WriteLine("They are equal");
+            return true;
+            
+        }
+
+        public static Employee operator!= (Employee employeeOne, Employee employeeTwo)
+        {
+            Console.WriteLine("They are not equal");
+            return false;
+        }
+    }
+}
---
+++ Operators Assignment Submission/Operators Assignment Submission/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment_Submission
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Instantiating 2 employee objects with property values
+            Employee employee1 = new Employee() { ID = "one", firstName = "Roger", lastName = "Lum" };
+            Employee employee2 = new Employee() { ID = "two", firstName = "Marie", lastName = "Hill" };
+
+            // Trying to compare ID rpoperty of each object
+            employee1 = employee1.ID == employee2.ID;
+            Console.ReadLine();
+        }
+    }
+}
