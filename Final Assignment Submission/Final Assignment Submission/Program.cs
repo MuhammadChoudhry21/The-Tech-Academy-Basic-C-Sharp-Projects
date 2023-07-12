43483abcce32bcf7d788a6e707ba567aac2a3252
MuhammadChoudhry21 <fazal.zoom@gmail.com>
2023-07-12 14:35:40 -0400

    final assignment submission

---
+++ Final Assignment Submission/Final Assignment Submission/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Linq;
+using System.Data.Entity;
+using System.Collections;
+using System.Collections.Generic;
+using Final_Assignment_Submission;
+
+namespace Final_Assignment_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Muhammad" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
+
+
+
